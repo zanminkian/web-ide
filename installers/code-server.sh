026e496ad6333852cdd7d2885a75620bd834dcc7
曾明健 <1187356256@qq.com>
2023-01-25 19:10:59 +0000

    feat: update code-server

--- installers/code-server.sh
+++ installers/code-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-CODE_SERVER_VERSION=4.8.3
+CODE_SERVER_VERSION=4.9.1
 wget https://github.com/coder/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server_${CODE_SERVER_VERSION}_amd64.deb
 dpkg -i code-server_${CODE_SERVER_VERSION}_amd64.deb
 rm -rf code-server_${CODE_SERVER_VERSION}_amd64.deb
