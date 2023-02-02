e2da4b17d95953ece247884cc629fa49e4932d2a
曾明健 <1187356256@qq.com>
2023-02-02 14:41:07 +0000

    feat(node, go): add cnpm and sort

--- installers/go.sh
+++ installers/go.sh
@@ -4,9 +4,9 @@ tar -xf /usr/local/bin/g1.3.0.linux-amd64.tar.gz -C /usr/local/bin
 rm -rf /usr/local/bin/g1.3.0.linux-amd64.tar.gz
 
 echo '\n# g' >> ~/.zshrc
-echo 'export GOROOT="${HOME}/.g/go"' >> ~/.zshrc
 echo 'export PATH="${HOME}/.g/go/bin:$PATH"' >> ~/.zshrc
 echo 'export G_MIRROR=https://golang.google.cn/dl/' >> ~/.zshrc
+echo 'export GOROOT="${HOME}/.g/go"' >> ~/.zshrc
 echo 'export GO111MODULE="on"' >> ~/.zshrc
 echo 'export GOPROXY="https://goproxy.cn,direct"' >> ~/.zshrc
 
--- installers/node.sh
+++ installers/node.sh
@@ -3,3 +3,6 @@ curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o /usr/local/bin/n
 chmod +x /usr/local/bin/n
 n 16
 npm i -g pnpm yarn ts-node tsx loadtest http-server pm2 cloc npm-check-updates
+
+echo '\n#node' >> ~/.zshrc
+echo 'alias cnpm="npm --registry=https://registry.npmmirror.com"' >> ~/.zshrc
