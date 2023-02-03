dd5fa7068d861b17e5227befffcfd51b87c0cc29
曾明健 <hellozmj@qq.com>
2023-02-03 07:41:00 +0000

    feat(go): add GOPATH

--- installers/go.sh
+++ installers/go.sh
@@ -4,9 +4,10 @@ tar -xf /usr/local/bin/g1.3.0.linux-amd64.tar.gz -C /usr/local/bin
 rm -rf /usr/local/bin/g1.3.0.linux-amd64.tar.gz
 
 echo '\n# go' >> ~/.zshrc
-echo 'export PATH="${HOME}/.g/go/bin:$PATH"' >> ~/.zshrc
 echo 'export G_MIRROR=https://golang.google.cn/dl/' >> ~/.zshrc
+echo 'export PATH="${HOME}/.g/go/bin:$PATH"' >> ~/.zshrc
 echo 'export GOROOT="${HOME}/.g/go"' >> ~/.zshrc
+echo 'export GOPATH="${HOME}/go"' >> ~/.zshrc
 echo 'export GO111MODULE="on"' >> ~/.zshrc
 echo 'export GOPROXY="https://goproxy.cn,direct"' >> ~/.zshrc
 
