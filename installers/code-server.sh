b5d634162932fcde3efb07b3f03f4770b08ead1f
zanminkian <hellozmj@qq.com>
2023-02-07 01:57:55 +0800

    feat(code-server): install cs in another way

--- installers/code-server.sh
+++ installers/code-server.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env zsh
 
 CODE_SERVER_VERSION=4.9.1
-wget https://github.com/coder/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server_${CODE_SERVER_VERSION}_amd64.deb
-dpkg -i code-server_${CODE_SERVER_VERSION}_amd64.deb
-rm -rf code-server_${CODE_SERVER_VERSION}_amd64.deb
+curl -L https://github.com/coder/code-server/releases/download/v$CODE_SERVER_VERSION/code-server-$CODE_SERVER_VERSION-linux-amd64.tar.gz -o /usr/lib/code-server.tgz
+tar -xzf /usr/lib/code-server.tgz -C /usr/lib
+mv /usr/lib/code-server-$CODE_SERVER_VERSION-linux-amd64 /usr/lib/code-server
+rm -rf /usr/lib/code-server.tgz
 
+CODE_SERVER_HOME=/usr/lib/code-server
 export EXTENSIONS_GALLERY='{"serviceUrl":"https://marketplace.visualstudio.com/_apis/public/gallery"}'
-code-server --install-extension GraphQL.vscode-graphql-syntax
-code-server --install-extension IronGeek.vscode-env
-code-server --install-extension christian-kohler.path-intellisense
-code-server --install-extension esbenp.prettier-vscode
-code-server --install-extension golang.Go
-code-server --install-extension humao.rest-client
-code-server --install-extension mhutchie.git-graph
-code-server --install-extension mrkou47.thrift-syntax-support
-code-server --install-extension ppz.ppz
-code-server --install-extension ryu1kn.partial-diff
-code-server --install-extension steoates.autoimport
-code-server --install-extension streetsidesoftware.code-spell-checker
-code-server --install-extension wmaurer.change-case
-code-server --install-extension njzy.stats-bar
-code-server --install-extension shardulm94.trailing-spaces
-code-server --install-extension saber2pr.file-git-history
-code-server --install-extension oderwat.indent-rainbow
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension GraphQL.vscode-graphql-syntax
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension IronGeek.vscode-env
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension christian-kohler.path-intellisense
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension esbenp.prettier-vscode
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension golang.Go
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension humao.rest-client
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension mhutchie.git-graph
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension mrkou47.thrift-syntax-support
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension ppz.ppz
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension ryu1kn.partial-diff
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension steoates.autoimport
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension streetsidesoftware.code-spell-checker
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension wmaurer.change-case
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension njzy.stats-bar
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension shardulm94.trailing-spaces
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension saber2pr.file-git-history
+$CODE_SERVER_HOME/lib/node $CODE_SERVER_HOME --install-extension oderwat.indent-rainbow
 
 # vscode setting
 echo '{"workbench.colorTheme":"Default Dark+","extensions.autoCheckUpdates":false,"extensions.autoUpdate":false,"extensions.ignoreRecommendations":true,"workbench.startupEditor":"none"}' > ~/.local/share/code-server/User/settings.json
