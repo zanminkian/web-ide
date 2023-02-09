febbbae4ea13e10c4f31a890dffd2e292fc6765a
zanminkian <hellozmj@qq.com>
2023-02-09 12:32:35 +0000

    ci: update release workflow

---
+++ .changeset/purple-doors-float.md
@@ -0,0 +1,5 @@
+---
+"code-server": patch
+---
+
+ci: update release workflow
--- installers/node.sh
+++ installers/node.sh
@@ -2,7 +2,7 @@
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o /usr/local/bin/n
 chmod +x /usr/local/bin/n
 n 16
-npm i -g pnpm ts-node tsx loadtest http-server pm2 cloc npm-check-updates @changesets/cli
+npm i -g pnpm ts-node tsx loadtest http-server pm2 cloc npm-check-updates
 
 echo '\n#node' >> ~/.zshrc
 echo 'alias cnpm="npm --registry=https://registry.npmmirror.com"' >> ~/.zshrc
