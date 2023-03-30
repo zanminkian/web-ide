72b9e7854d1f5d37a8844c85e3bfb986325a124d
zanminkian <hellozmj@qq.com>
2023-03-30 07:29:53 +0000

    feat(node): corepack prepare

--- src/installers/node.sh
+++ src/installers/node.sh
@@ -6,6 +6,7 @@ curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o /usr/local/bin/n
 chmod +x /usr/local/bin/n
 n 16
 corepack enable
+corepack prepare --all
 npm i -g ts-node tsx loadtest http-server pm2 cloc npm-check-updates
 
 
