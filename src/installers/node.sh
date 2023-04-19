49537eb9c0f3077dcb1cbac383b249de57d2720d
zanminkian <hellozmj@qq.com>
2023-04-19 20:02:12 +0000

    feat(node): install degit

--- src/installers/node.sh
+++ src/installers/node.sh
@@ -7,7 +7,7 @@ chmod +x /usr/local/bin/n
 n 16
 corepack enable
 corepack prepare --all
-npm i -g ts-node tsx loadtest http-server pm2 cloc npm-check-updates
+npm i -g ts-node tsx http-server pm2 npm-check-updates degit loadtest cloc
 
 
 echo '# node
