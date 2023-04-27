f8a1e3ffe8a265daa2e6e829cc84a1e94d11d890
zanminkian <hellozmj@qq.com>
2023-04-27 13:11:44 +0000

    refactor(go): optimize go installer

--- src/installers/go.sh
+++ src/installers/go.sh
@@ -6,6 +6,11 @@ wget https://github.com/voidint/g/releases/download/v1.3.0/g1.3.0.linux-amd64.ta
 tar -xf /usr/local/bin/g1.3.0.linux-amd64.tar.gz -C /usr/local/bin
 rm -rf /usr/local/bin/g1.3.0.linux-amd64.tar.gz
 
+/usr/local/bin/g install 1.19.4
+"${HOME}/.g/go/bin/go" install -v golang.org/x/tools/gopls@v0.11.0
+"${HOME}/.g/go/bin/go" install -v github.com/go-delve/delve/cmd/dlv@v1.20.1
+"${HOME}/.g/go/bin/go" install -v honnef.co/go/tools/cmd/staticcheck@v0.4.3
+
 echo '# go
 export G_MIRROR=https://golang.google.cn/dl/
 export PATH="${HOME}/.g/go/bin:$PATH"
@@ -14,10 +19,3 @@ export GOPATH="${HOME}/go"
 export GO111MODULE="on"
 export GOPROXY="https://goproxy.cn,direct"
 ' >> ~/.zshrc
-
-/usr/local/bin/g install 1.19.4
-. ~/.zshrc
-
-go install -v golang.org/x/tools/gopls@v0.11.0
-go install -v github.com/go-delve/delve/cmd/dlv@v1.20.1
-go install -v honnef.co/go/tools/cmd/staticcheck@v0.4.3
