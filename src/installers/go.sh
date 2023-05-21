fb27ac97254043623eca9c2eac7c1cc96031c72c
zanminkian <hellozmj@qq.com>
2023-05-21 20:02:54 +0000

    feat(go): rename g to gvm

--- src/installers/go.sh
+++ src/installers/go.sh
@@ -5,8 +5,9 @@ set -e
 wget https://github.com/voidint/g/releases/download/v1.5.0/g1.5.0.linux-amd64.tar.gz -P /usr/local/bin
 tar -xf /usr/local/bin/g1.5.0.linux-amd64.tar.gz -C /usr/local/bin
 rm -rf /usr/local/bin/g1.5.0.linux-amd64.tar.gz
+mv /usr/local/bin/g /usr/local/bin/gvm
 
-/usr/local/bin/g install 1.19.4
+gvm install 1.19.4
 "${HOME}/.g/go/bin/go" install -v golang.org/x/tools/gopls@v0.11.0
 "${HOME}/.g/go/bin/go" install -v github.com/go-delve/delve/cmd/dlv@v1.20.1
 "${HOME}/.g/go/bin/go" install -v honnef.co/go/tools/cmd/staticcheck@v0.4.3
