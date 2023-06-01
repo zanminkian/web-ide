fbce734738f8bb310e63d7116123f91850629e8c
zanminkian <hellozmj@qq.com>
2023-06-01 05:09:01 +0000

    chore: simplify the release script

---
+++ .changeset/great-jobs-rest.md
@@ -0,0 +1,5 @@
+---
+"web-ide": patch
+---
+
+chore: simplify the release script
--- release.sh
+++ release.sh
@@ -1,11 +1,6 @@
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fLsS https://get.docker.com/ | sh
 
 TAG_NAME=$(node -e 'console.log(require("./package.json").version)')
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -f Dockerfile.build -t zengmingjian/web-ide:latest -t zengmingjian/web-ide:$TAG_NAME .
-docker push zengmingjian/web-ide:$TAG_NAME
+docker push zengmingjian/web-ide:latest zengmingjian/web-ide:$TAG_NAME
