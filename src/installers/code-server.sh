8a50c34ccd59afaebe9715ec58a74e53ba7c3327
zanminkian <hellozmj@qq.com>
2023-06-27 03:25:47 +0000

    chore: sort the installation of vscode extensions

--- src/installers/code-server.sh
+++ src/installers/code-server.sh
@@ -8,14 +8,14 @@ rm -rf code-server_${CODE_SERVER_VERSION}_amd64.deb
 
 # Lock extensions' version once https://github.com/microsoft/vscode/issues/172917 has been resolved.
 export EXTENSIONS_GALLERY='{"serviceUrl":"https://marketplace.visualstudio.com/_apis/public/gallery"}'
-code-server --install-extension GraphQL.vscode-graphql-syntax
-code-server --install-extension IronGeek.vscode-env
-code-server --install-extension christian-kohler.path-intellisense
 code-server --install-extension golang.Go@0.38.0
 code-server --install-extension humao.rest-client@0.25.1 # hard to use. change to httpYac one day
 code-server --install-extension mhutchie.git-graph@1.30.0
-code-server --install-extension mrkou47.thrift-syntax-support
 code-server --install-extension ryu1kn.partial-diff@1.4.3
+code-server --install-extension GraphQL.vscode-graphql-syntax
+code-server --install-extension IronGeek.vscode-env
+code-server --install-extension christian-kohler.path-intellisense
+code-server --install-extension mrkou47.thrift-syntax-support
 code-server --install-extension steoates.autoimport # hard to use. remove it one day
 code-server --install-extension streetsidesoftware.code-spell-checker
 code-server --install-extension wmaurer.change-case
@@ -26,7 +26,6 @@ code-server --install-extension pbkit.vscode-pbkit
 code-server --install-extension usernamehw.errorlens
 code-server --install-extension wwm.better-align
 # code-server --install-extension ppz.ppz # some error logs appear
-# code-server --install-extension saber2pr.file-git-history # Vscode has integrated time-line. It's unnecessary.
 
 # vscode setting
 echo '{
