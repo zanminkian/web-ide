e4ad585e318303534f03e4fa1510a9ae73935591
zanminkian <hellozmj@qq.com>
2023-08-13 03:08:12 +0000

    refactor(): move file

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@ curl -fLsS https://get.docker.com/ | sh
 
 TAG_NAME=$(node -e 'console.log(require("./package.json").version)')
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -f Dockerfile.build -t zengmingjian/web-ide:latest -t zengmingjian/web-ide:$TAG_NAME .
+docker build -f src/Dockerfile -t zengmingjian/web-ide:latest -t zengmingjian/web-ide:$TAG_NAME .
 docker push -a zengmingjian/web-ide
