3d0cbd83058d8f11068c880875e79d4b2b03a050
zanminkian <hellozmj@qq.com>
2023-08-13 04:43:13 +0000

    build(): fix build error

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@ curl -fLsS https://get.docker.com/ | sh
 
 TAG_NAME=$(node -e 'console.log(require("./package.json").version)')
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -f src/Dockerfile -t zengmingjian/web-ide:latest -t zengmingjian/web-ide:$TAG_NAME .
+docker build -f src/Dockerfile -t zengmingjian/web-ide:latest -t zengmingjian/web-ide:$TAG_NAME src
 docker push -a zengmingjian/web-ide
