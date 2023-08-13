fee081ea7588f23647b68c6ba34416b08b87b006
zanminkian <hellozmj@qq.com>
2023-08-13 03:15:53 +0000

    feat(go,ttyd): upgrade

--- src/installers/ttyd.sh
+++ src/installers/ttyd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 set -e
 
-curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.2/ttyd.x86_64 -o /usr/local/bin/ttyd
+curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64 -o /usr/local/bin/ttyd
 chmod +x /usr/local/bin/ttyd
