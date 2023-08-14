ce75bf5133a45ed2a751e3c24353bba243c3c8e6
zanminkian <hellozmj@qq.com>
2023-08-14 10:56:12 +0000

    feat(cli-tools): instal mycli

---
+++ .changeset/warm-spiders-tan.md
@@ -0,0 +1,5 @@
+---
+"web-ide": patch
+---
+
+feat(cli-tools): instal mycli
--- src/installers/cli-tools.sh
+++ src/installers/cli-tools.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 set -e
 
+# install ttyd
 curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64 -o /usr/local/bin/ttyd
 chmod +x /usr/local/bin/ttyd
+
+## install nginx and mysql client
+apt update
+apt install -y nginx mycli
