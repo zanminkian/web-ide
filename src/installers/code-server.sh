d09c34aac725facd4b404691910982db403de076
zanminkian <hellozmj@qq.com>
2023-08-30 09:24:14 +0000

    chore: update

--- src/installers/code-server.sh
+++ src/installers/code-server.sh
@@ -6,17 +6,13 @@ wget https://github.com/coder/code-server/releases/download/v${CODE_SERVER_VERSI
 dpkg -i code-server_${CODE_SERVER_VERSION}_amd64.deb
 rm -rf code-server_${CODE_SERVER_VERSION}_amd64.deb
 
-# Lock extensions' version once https://github.com/microsoft/vscode/issues/172917 has been resolved.
 export EXTENSIONS_GALLERY='{"serviceUrl":"https://marketplace.visualstudio.com/_apis/public/gallery"}'
 code-server --install-extension golang.Go@0.38.0
-code-server --install-extension humao.rest-client@0.25.1 # hard to use. change to httpYac one day
 code-server --install-extension mhutchie.git-graph@1.30.0
 code-server --install-extension ryu1kn.partial-diff
 code-server --install-extension GraphQL.vscode-graphql-syntax
 code-server --install-extension IronGeek.vscode-env
 code-server --install-extension christian-kohler.path-intellisense
-code-server --install-extension mrkou47.thrift-syntax-support
-code-server --install-extension steoates.autoimport # hard to use. remove it one day
 code-server --install-extension streetsidesoftware.code-spell-checker
 code-server --install-extension wmaurer.change-case
 code-server --install-extension njzy.stats-bar
@@ -24,6 +20,9 @@ code-server --install-extension shardulm94.trailing-spaces
 code-server --install-extension oderwat.indent-rainbow
 code-server --install-extension pbkit.vscode-pbkit
 code-server --install-extension wwm.better-align
+code-server --install-extension humao.rest-client # not perfect
+code-server --install-extension steoates.autoimport # hard to use. remove it one day
+code-server --install-extension mrkou47.thrift-syntax-support
 # code-server --install-extension usernamehw.errorlens # too noisy
 # code-server --install-extension ppz.ppz # some error logs appear
 
