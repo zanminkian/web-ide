7754e508637dc73c0cb2f728782104a63c32fbee
zanminkian <hellozmj@qq.com>
2023-09-28 03:55:58 +0000

    refactor: optimize file stucture

--- src/installers/cli-tools.sh
+++ src/installers/cli-tools.sh
@@ -5,6 +5,6 @@ set -e
 curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64 -o /usr/local/bin/ttyd
 chmod +x /usr/local/bin/ttyd
 
-## install nginx and mysql client
+## install tools via apt
 apt update
-apt install -y nginx mysql-client
+apt install -y nginx mysql-client tree
---
+++ src/installers/deprecated.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+set -e
+
+# install something which will be remove in the future
+apt update
+apt install -y krb5-user
