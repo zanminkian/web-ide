e515b863f7c33e0d605a06ae2665093fd7a66b21
曾明健 <hellozmj@qq.com>
2023-12-05 07:02:57 +0000

    refactor!: remove go, kerberos, python and ttyd

---
+++ .changeset/mighty-falcons-refuse.md
@@ -0,0 +1,5 @@
+---
+"web-ide": patch
+---
+
+refactor!: remove go, kerberos, python and ttyd
--- src/installers/cli-tools.installer.sh
+++ src/installers/cli-tools.installer.sh
@@ -2,8 +2,8 @@
 set -e
 
 # install ttyd
-curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64 -o /usr/local/bin/ttyd
-chmod +x /usr/local/bin/ttyd
+# curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64 -o /usr/local/bin/ttyd
+# chmod +x /usr/local/bin/ttyd
 
 ## install tools via apt
 apt update
