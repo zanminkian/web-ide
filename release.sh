75c8bd2353297f372d115e10c3091fcc4ac0e5ba
曾明健 <hellozmj@qq.com>
2023-12-05 07:24:08 +0000

    build: add --platform option when building

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ curl -fLsS https://get.docker.com/ | sh
 VERSION=$(node -e 'console.log(require("./package.json").version)')
 TAG=$(node -e 'console.log(require("./package.json").version.match(/^[0-9]+\.[0-9]+\.[0-9]+-(.+)\.[0-9]+$/)?.[1] || "latest")')
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+docker buildx ls
+docker build --platform linux/arm64 -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
 docker images -a
 docker push -a zengmingjian/web-ide
