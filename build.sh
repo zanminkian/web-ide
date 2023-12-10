99464ee1013fae0ccc313ed614c6d944b8f049ef
zanminkian <hellozmj@qq.com>
2023-12-10 11:39:46 +0000

    build: add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+curl -fLsS https://get.docker.com/ | sh
+
+VERSION=$(node -e 'console.log(require("./package.json").version)')
+TAG=$(node -e 'console.log(require("./package.json").version.match(/^[0-9]+\.[0-9]+\.[0-9]+-(.+)\.[0-9]+$/)?.[1] || "latest")')
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker buildx create --use --name mybuilder
+docker buildx inspect mybuilder --bootstrap
+docker buildx build --platform linux/amd64,linux/arm64 -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+docker images -a
--- release.sh
+++ release.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-curl -fLsS https://get.docker.com/ | sh
-
-VERSION=$(node -e 'console.log(require("./package.json").version)')
-TAG=$(node -e 'console.log(require("./package.json").version.match(/^[0-9]+\.[0-9]+\.[0-9]+-(.+)\.[0-9]+$/)?.[1] || "latest")')
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker buildx ls
-docker buildx create --use --name mybuilder
-docker buildx inspect mybuilder --bootstrap
-docker buildx ls
-docker buildx build --platform linux/amd64,linux/arm64 --push -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+./build.sh
+echo "build success!"
+docker push -a zengmingjian/web-ide
