d2d5fed2fa7e65965c3aa90c351a2bd7d9800446
zanminkian <hellozmj@qq.com>
2024-06-17 14:18:38 +0000

    fix: remove WI env when running for safety

---
+++ .changeset/nasty-rockets-sort.md
@@ -0,0 +1,5 @@
+---
+"web-ide": patch
+---
+
+fix: remove WI env when running for safety
--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env zsh
 
-TARGET_DIR=$(cd "$(dirname "$0")"; pwd)/installers
+echo '# web-ide
+eval "$(web-ide-env)"
+' >> ~/.zshrc
 
+TARGET_DIR=$(cd "$(dirname "$0")"; pwd)/installers
 for file in $TARGET_DIR/*.installer.sh; do
   echo "[$(arch)]executing: $file"
   $file || { echo "[$(arch)]execute fail: $file" && exit 1; }
