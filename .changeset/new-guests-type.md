ceba6cd302ad791388ffec5138add39b4348a7da
zanminkian <hellozmj@qq.com>
2024-06-17 14:13:23 +0000

    feat(omz): no need to place omz at the top

---
+++ .changeset/new-guests-type.md
@@ -0,0 +1,5 @@
+---
+"web-ide": patch
+---
+
+feat(omz): no need to place omz at the top
--- src/installers/omz.installer.sh
+++ src/installers/omz.installer.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env zsh
 set -e
 
-touch ~/.zshrc
-mv ~/.zshrc ~/.zshrc_tmp
-
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
@@ -12,7 +9,6 @@ cp ~/.oh-my-zsh/themes/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/simple-robbyru
 sed -i 's/%c/%~/' ~/.oh-my-zsh/themes/simple-robbyrussell.zsh-theme
 sed -i 's/git://' ~/.oh-my-zsh/themes/simple-robbyrussell.zsh-theme
 
-rm -rf ~/.zshrc
 echo '# omz
 export ZSH="$HOME/.oh-my-zsh"
 DISABLE_AUTO_UPDATE="true" # https://stackoverflow.com/questions/11378607/oh-my-zsh-disable-would-you-like-to-check-for-updates-prompt
@@ -20,6 +16,3 @@ ZSH_THEME="simple-robbyrussell"
 plugins=(zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
 ' >> ~/.zshrc
-
-cat ~/.zshrc_tmp >> ~/.zshrc
-rm -rf ~/.zshrc_tmp
