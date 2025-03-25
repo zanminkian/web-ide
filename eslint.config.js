f77bf256e91f8e33ae237a43b0ffe285d02af7a5
zanminkian <hellozmj@qq.com>
2025-03-25 06:13:46 +0000

    build: optimize release

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-// TODO: Remove this file
-import { Builder } from "fenge/eslint-config";
-
-export default new Builder()
-  .enableJavaScript()
-  .enablePackageJson({
-    omit: ["publint/warning", "publint/error"],
-  })
-  .toConfig();
--- release.sh
+++ release.sh
@@ -5,7 +5,9 @@ curl -fLsS https://get.docker.com/ | sh
 VERSION=$(node -e 'console.log(require("./package.json").version)')
 TAG=$(node -e 'console.log(require("./package.json").version.match(/^[0-9]+\.[0-9]+\.[0-9]+-(.+)\.[0-9]+$/)?.[1] || "latest")')
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker buildx create --use --name mybuilder
 docker buildx inspect mybuilder --bootstrap
-docker buildx build --build-arg VERSION=$VERSION --platform linux/amd64,linux/arm64 --push -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+docker buildx build --build-arg VERSION=$VERSION --platform linux/amd64 -o type=tar,dest=- -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src | gzip > web-ide-$VERSION-linux-amd64.tgz
+ls -alh
+docker buildx build --build-arg VERSION=$VERSION --platform linux/arm64 -o type=tar,dest=- -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src | gzip > web-ide-$VERSION-linux-arm64.tgz
+ls -alh
