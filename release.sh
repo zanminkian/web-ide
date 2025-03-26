c90a344a0f143a68cee31281c62bdb408719a87e
zanminkian <hellozmj@qq.com>
2025-03-26 07:36:03 +0000

    chore: update build script

--- release.sh
+++ release.sh
@@ -7,11 +7,18 @@ TAG=$(node -e 'console.log(require("./package.json").version.match(/^[0-9]+\.[0-
 
 docker buildx create --use --name mybuilder
 docker buildx inspect mybuilder --bootstrap
-docker buildx build --build-arg VERSION=$VERSION --platform linux/amd64 -o type=tar,dest=- -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src | gzip > web-ide-$VERSION-linux-amd64.tgz
-ls -alh
-docker buildx build --build-arg VERSION=$VERSION --platform linux/arm64 -o type=tar,dest=- -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src | gzip > web-ide-$VERSION-linux-arm64.tgz
-ls -alh
 
+docker buildx build --build-arg VERSION=$VERSION --platform linux/amd64 --load -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+docker images -a
+docker save zengmingjian/web-ide:$VERSION | gzip > web-ide-$VERSION-linux-amd64.tgz
+docker image rm zengmingjian/web-ide:$VERSION
+
+docker buildx build --build-arg VERSION=$VERSION --platform linux/arm64 --load -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+docker images -a
+docker save zengmingjian/web-ide:$VERSION | gzip > web-ide-$VERSION-linux-arm64.tgz
+docker image rm zengmingjian/web-ide:$VERSION
+
+ls -alh
 mkdir assets
 mv web-ide-$VERSION-linux-amd64.tgz assets
 mv web-ide-$VERSION-linux-arm64.tgz assets
