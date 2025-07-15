24d1f85b18d1991eb985c8d19b2fc612da97a1c0
zanminkian <hellozmj@qq.com>
2025-07-15 12:33:38 +0000

    docs: update refresh.sh

--- doc/refresh.sh
+++ doc/refresh.sh
@@ -2,23 +2,21 @@
 # set -ex
 
 mkdir -p $HOME/web-ide-root/projects
-touch $HOME/web-ide-root/.zsh_history
 
 docker stop web-ide
 docker container rm -v web-ide
 docker run -itd \
   --network host \
   -e PASSWORD="MY_PASSWORD" \
-  -e WI_SSH_ID_ED25519_PUB="MY_PUBLICK_KEY" \
+  -e WI_SSH_ID_ED25519_PUB="MY_PUBLIC_KEY" \
   -e WI_SSH_ID_ED25519="MY_PRIVATE_KEY" \
   -e WI_SSH_KEYSCAN="github.com" \
   -e WI_GIT_CONFIG="[user]\n\tname = zanminkian\n\temail = hellozmj@qq.com\n[safe]\n\tdirectory = *\n" \
-  -e WI_INIT_CMD="echo 'registry=https://registry.npmmirror.com' >> ~/.npmrc" \
+  -e WI_INIT_CMD="echo 'registry=https://registry.npmmirror.com' >> ~/.npmrc; echo 'HISTFILE=~/projects/.zsh_history' >> ~/.zshrc" \
   -v $HOME/web-ide-root/projects:/root/projects \
-  -v $HOME/web-ide-root/.zsh_history:/root/.zsh_history \
   --restart always \
   --name web-ide \
-  zengmingjian/web-ide
+  zengmingjian/web-ide:0.22.2
 
 echo '============================='
 docker ps -a
