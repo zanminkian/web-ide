a38ab8ec0d06fd3b6acd1242f27e63531f3731f7
zanminkian <hellozmj@qq.com>
2025-07-21 18:05:44 +0000

    chore: remove all libs about electron

--- src/installers/cli.installer.sh
+++ src/installers/cli.installer.sh
@@ -11,9 +11,9 @@ apt install -y redis-tools
 
 # `build-essential` is required for some c/c++ libraries. Some Node projects (like some sub-libs of midway.js) using c++ needs this.
 # Others libs are required when building electron app. Refer to https://github.com/electron/forge/blob/35e7b9cd58cf0923dde58f94f477b09b5597f5ae/.circleci/config.yml#L152
-apt install -y --no-install-recommends \
-    build-essential \
-    libgtk-3-0 \
-    libnss3 \
-    libgbm1 \
-    libasound2
+# apt install -y --no-install-recommends \
+#     build-essential \
+#     libgtk-3-0 \
+#     libnss3 \
+#     libgbm1 \
+#     libasound2
