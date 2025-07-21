b4b64e392faea7c7402d815e33c63e6c882d7322
zanminkian <hellozmj@qq.com>
2025-07-21 06:59:46 +0000

    chore: update comments

--- src/installers/cli.installer.sh
+++ src/installers/cli.installer.sh
@@ -9,7 +9,7 @@ set -e
 apt install -y mysql-client redis-tools
 
 # `build-essential` is required for some c/c++ libraries. Some Node projects (like some sub-libs of midway.js) using c++ needs this.
-# Others libs are required when building electron app
+# Others libs are required when building electron app. Refer to https://github.com/electron/forge/blob/35e7b9cd58cf0923dde58f94f477b09b5597f5ae/.circleci/config.yml#L152
 apt install -y --no-install-recommends \
     build-essential \
     libgtk-3-0 \
