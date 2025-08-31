30a9dafc0fc272642e8e2f8088da8e6a6d34f280
zanminkian <hellozmj@qq.com>
2025-08-31 17:13:27 +0000

    refactor: rename files

--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 TARGET_DIR=$(cd "$(dirname "$0")"; pwd)/installers
-for file in $TARGET_DIR/*.installer.sh; do
+for file in $TARGET_DIR/*.sh; do
   echo "[$(arch)]executing: $file"
   $file || { echo "[$(arch)]execute fail: $file" && exit 1; }
   echo "[$(arch)]executed: $file"
