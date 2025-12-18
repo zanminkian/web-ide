2c35f297c284bf54307395c1a95d61252ca56b20
zanminkian <hellozmj@qq.com>
2025-12-18 11:01:36 +0000

    build: optimize release script

--- release.sh
+++ release.sh
@@ -9,17 +9,16 @@ else
 fi
 
 VERSION=$(node -e 'console.log(require("./package.json").version)')
-TAG=$(node -e 'console.log(require("./package.json").version.match(/^[0-9]+\.[0-9]+\.[0-9]+-(.+)\.[0-9]+$/)?.[1] || "latest")')
 
 docker buildx create --use --name mybuilder
 docker buildx inspect mybuilder --bootstrap
 
-docker buildx build --build-arg VERSION=$VERSION --platform linux/amd64 --load -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+docker buildx build --build-arg VERSION=$VERSION --platform linux/amd64 --load -f src/Dockerfile -t zengmingjian/web-ide:$NPM_CONFIG_TAG -t zengmingjian/web-ide:$VERSION src
 docker images -a
 docker save zengmingjian/web-ide:$VERSION | gzip > web-ide-$VERSION-linux-amd64.tgz
 docker image rm zengmingjian/web-ide:$VERSION
 
-docker buildx build --build-arg VERSION=$VERSION --platform linux/arm64 --load -f src/Dockerfile -t zengmingjian/web-ide:$TAG -t zengmingjian/web-ide:$VERSION src
+docker buildx build --build-arg VERSION=$VERSION --platform linux/arm64 --load -f src/Dockerfile -t zengmingjian/web-ide:$NPM_CONFIG_TAG -t zengmingjian/web-ide:$VERSION src
 docker images -a
 docker save zengmingjian/web-ide:$VERSION | gzip > web-ide-$VERSION-linux-arm64.tgz
 docker image rm zengmingjian/web-ide:$VERSION
