2c3b2e2706d8b366d721f8a1edca47919b5c3090
zanminkian <hellozmj@qq.com>
2026-01-24 13:23:11 +0000

    chore: refresh

--- src/settings/node.sh
+++ src/settings/node.sh
@@ -16,11 +16,11 @@ npm i -g @rnm/gpp
 
 jrm use node@20
 npm i -g @rnm/pm
-pm-cli enable-shim
+pm-util enable-shim
 
 jrm use node@22
 npm i -g @rnm/pm
-pm-cli enable-shim
+pm-util enable-shim
 
 # TODO: Remove this if this [issue](https://github.com/pnpm/pnpm/issues/5803) is solved.
 # Refer: https://github.com/pnpm/pnpm/issues/7024#issuecomment-1740740451.
