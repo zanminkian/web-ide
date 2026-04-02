15c523fe9d21fa1d064760b97aa3435f2162d1e6
zanminkian <hellozmj@qq.com>
2026-04-02 03:57:52 +0000

    build: dynamic install lts versions

---
+++ .changeset/yellow-brooms-hammer.md
@@ -0,0 +1,5 @@
+---
+"web-ide": patch
+---
+
+build: dynamic install lts versions
--- src/settings/node.sh
+++ src/settings/node.sh
@@ -1,18 +1,63 @@
 #!/usr/bin/env zsh
 set -e
 
+# Get maintaining LTS versions (from newest to oldest)
+get_maintaining_lts_versions() {
+  local current_date=$(date +%Y-%m-%d)
+
+  curl -s https://raw.githubusercontent.com/nodejs/Release/main/schedule.json | \
+  awk -v current_date="$current_date" '
+  /"v[0-9]+"/ {
+    # Extract version number
+    match($0, /"v([0-9]+)"/)
+    version = substr($0, RSTART+2, RLENGTH-3)
+    in_version = 1
+    lts = ""
+    end = ""
+    next
+  }
+
+  in_version && /"lts":/ {
+    # Extract lts date
+    match($0, /"lts": "([^"]+)"/)
+    lts = substr($0, RSTART+8, RLENGTH-9)
+  }
+
+  in_version && /"end":/ {
+    # Extract end date
+    match($0, /"end": "([^"]+)"/)
+    end = substr($0, RSTART+8, RLENGTH-9)
+  }
+
+  in_version && /^  \}/ {
+    # End of version block
+    if (lts != "" && lts <= current_date && end > current_date) {
+      print version
+    }
+    in_version = 0
+  }
+  ' | sort -rn
+}
+
+# Get and install all maintaining LTS versions
+lts_versions=($(get_maintaining_lts_versions))
+echo "Detected maintaining LTS versions: ${lts_versions[@]}"
+
 eval "$(jrm env)"
 
-jrm install node@22
-jrm install node@20
+# Install each version
+for version in "${lts_versions[@]}"; do
+  echo "Installing Node.js $version..."
+  jrm install node@$version
+done
 
-jrm use node@20
-npm i -g @rnm/pm
-pm-util enable-shim
-
-jrm use node@22
-npm i -g @rnm/pm
-pm-util enable-shim
+# Configure each installed version (in reverse order, from oldest to newest)
+for version in "${(Oa)lts_versions[@]}"; do
+  echo "Configuring Node.js $version..."
+  jrm use node@$version
+  npm i -g @rnm/pm
+  pm-util enable-shim
+done
 
 # TODO: Remove this if this [issue](https://github.com/pnpm/pnpm/issues/5803) is solved.
 # Refer: https://github.com/pnpm/pnpm/issues/7024#issuecomment-1740740451.
